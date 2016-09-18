fdb7519d47612a7dcaf5430c63c70df5cfb58f94
Tanner Olsen <olsen.tanner.b@gmail.com>
2016-09-18 13:54:51 -0600

    Completed Deck class

--- 21CardTrick/21CardTrick/Deck.cs
+++ 21CardTrick/21CardTrick/Deck.cs
@@ -8,5 +8,79 @@ namespace _21CardTrick
 {
     class Deck
     {
+        //variables
+        //constructor
+        //create deck array[52]
+        //add cards to deck
+        //shuffle deck
+        //shuffle function
+
+
+        //private static Random rng = new Random();
+
+        //public static void Shuffle<T>(this IList<T> list)
+        //{
+        //    int n = list.Count;
+        //    while (n > 1)
+        //    {
+        //        n--;
+        //        int k = rng.Next(n + 1);
+        //        T value = list[k];
+        //        list[k] = list[n];
+        //        list[n] = value;
+        //    }
+        //}
+
+
+        //Variables
+        private Card[] fullDeck;
+        private Card[] deckOf21;
+        private int cardCount = 0;
+        private static Random rng = new Random();
+
+        //Constructor
+        public Deck()
+        {   
+            //Create 52 card deck
+            fullDeck = new Card[52];
+
+            //Populate deck with cards
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int face = 0; face < 13; face++)
+                {
+                    fullDeck[cardCount++] = new Card(face, suit);
+                }
+            }
+
+            //Shuffle deck
+            Shuffle();
+
+            //Return 21 cards
+            Random21();
+        }
+
+        public void Shuffle()
+        {
+            int n = fullDeck.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = fullDeck[k];
+                fullDeck[k] = fullDeck[n];
+                fullDeck[n] = value;
+            }
+        }
+
+        public Card[] Random21()
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                deckOf21[i] = fullDeck[i];
+            }
+
+            return deckOf21;
+        }
     }
 }
