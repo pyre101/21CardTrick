556257379878a1ebd100bbf4eeb4d1f092f02f59
Adam Farrell <adamfarrell@mail.weber.edu>
2016-09-18 16:32:38 -0600

    Added Player class for Mashael

--- 21CardTrick/21CardTrick/Player.cs
+++ 21CardTrick/21CardTrick/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,39 @@ namespace _21CardTrick
 {
     class Player
     {
+        public bool hasCardSelected;
+        private Card selectedCard;
+
+
+        public Column indicateCouloumn(Column[] coloumns, int coulmnNumber)
+        {
+            return coloumns[coulmnNumber];
+        }
+
+
+        public Card selectCard(Card.CardSuit suit, int Value)
+        {
+            if (!hasCardSelected)
+            {
+                Card C = new Card();
+                C.Value = Value;
+                C.Suit = suit;
+                hasCardSelected = true;
+                selectedCard = C;
+            }
+
+            return selectedCard;
+        }
+
+
+        public bool isCorrectCard(Card card)
+        {
+            if (card.Value == selectedCard.Value && card.Suit == selectedCard.Suit)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
